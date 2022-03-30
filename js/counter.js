3d2ad7c46d0434344721b7c8acf76c18addda497
tonyatayi <aatayi@outlook.com>
2022-03-30 20:40:50 +0100

    counter

--- js/counter.js
+++ js/counter.js
@@ -1,20 +1,13 @@
-jQuery(document).ready(function () {
-    function count($this) {
-        var current = parseInt($this.html(), 10);
-        current = current + 1; /* Where 1 is increment */
-        $this.html(++current);
-        if (current > $this.data("count")) {
-            $this.html($this.data("count"));
-        } else {
-            setTimeout(function () {
-                count($this);
-            }, 50);
-        }
-    }
-
-    jQuery(".stat-count").each(function () {
-        jQuery(this).data("count", parseInt(jQuery(this).html(), 10));
-        jQuery(this).html("0");
-        count(jQuery(this));
+$(document).ready(function () {
+    $('.counter-number').each(function () {
+        $(this).prop('counter', 0).animate({
+            counter: $(this).text()
+        }, {
+            duration: 3500,
+            ease: 'swin',
+            step: function (now) {
+                $(this).text(Math.floor(now) + '+');
+            }
+        });
     });
 });
